0fb1af75a5b7b1125a23032818229dd8fc09a008
CamTheWebGuy <cameronanchondo@yahoo.com>
2022-02-18 22:04:55 -0600

    - Added Header w/ Logo & Searchbar  - Added Hero Banner w/ CTA  - Added Search Dates button

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { DateRangePicker } from 'react-date-range';
+// Main style file
+import 'react-date-range/dist/styles.css';
+// Theme CSS file
+import 'react-date-range/dist/theme/default.css';
+
+import './Banner.css';
+
+function Search() {
+  return <div className='search'></div>;
+}
+
+export default Search;
