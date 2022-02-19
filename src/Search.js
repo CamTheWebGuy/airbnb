dafeaabdbfa4d72d2a1460a77b416868cfe0b203
CamTheWebGuy <cameronanchondo@yahoo.com>
2022-02-18 22:18:52 -0600

    - Added React Date Range  - Added Date-FNS Package

--- src/Search.js
+++ src/Search.js
@@ -8,7 +8,25 @@ import 'react-date-range/dist/theme/default.css';
 import './Banner.css';
 
 function Search() {
-  return <div className='search'></div>;
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  return (
+    <div className='search'>
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default Search;
